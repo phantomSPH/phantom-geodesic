9bb85de60a076810cdf0d514affe66bec78d2be8
David Liptai <david.liptai@monash.edu>
2016-05-20 17:17:22 +1000

    Added conservative to primitive solver. Removed get_v_from_p (it was wrong).

--- cons2prim.f90
+++ cons2prim.f90
@@ -40,12 +40,31 @@ contains
 !+
 !----------------------------------------------------------------
 subroutine primitive2conservative(x,den,v,u,P,rho,pmom,en)
-! use metric,   only:get_metric
-! use utils_gr, only:dot_product_gr
+ use utils_gr, only: dot_product_gr
+ use metric, only: get_metric
  real, intent(in)  :: x(1:3)
- real, intent(in)  :: den,v(1:3),u,P
- real, intent(out) :: rho,pmom(1:3),en
+ real, intent(in) :: den,v(1:3),u,P
+ real, intent(out)  :: rho,pmom(1:3),en
+ real, dimension(0:3,0:3) :: gcov, gcon
+ real :: sqrtg, H, gimuvmu, gimuvmuvi, U0, v4(0:3)
+ integer :: i, mu
+ v4 = (/1.,v(1:3)/)
+ H = 1.+ u + P/den
+ call get_metric(x,gcov,gcon,sqrtg)
+ U0  = 1./sqrt(-dot_product_gr(v,v,gcov))
 
+ rho = sqrtg*den*U0
+ do i=1,3
+    gimuvmu = dot_product(gcov(i,:),v4(:))
+    pmom(i) = U0*H*gimuvmu
+ enddo
+ gimuvmuvi = 0.
+ do i=1,3
+    do mu=1,4
+       gimuvmuvi = gimuvmuvi + gcov(i,mu)*v(mu)*v(i)
+    enddo
+ enddo
+ en = U0*(H*gimuvmuvi - (1.+u)*dot_product_gr(v,v,gcov))
 ! call get_metric(ndim,x,gcov,gcon,sqrtg)
 
 ! Lorentz = 1./sqrt(1. + dot_product_gr(v,v,gcov))
@@ -82,14 +101,9 @@ subroutine get_v_from_p(pmom,v,x)
  real, dimension(0:3,0:3) :: gcon,gcov
  real :: w, U0, sqrtg, gvv
  integer :: i
- call get_metric(x,gcov,gcon,sqrtg)
- gvv = dot_product((/ (dot_product(gcov(:,i),fourv),i=0,3) /),fourv)
- U0  = 1./sqrt(-gvv)
- w   = 1.
- fourv = 1./(U0*w)*(/ (dot_product(gcon(i,1:3),pmom),i=0,3) /)
- v = fourv(1:3)
 
  !call conservative2primitive(x,v,0.,0.,0.,rho,pmom,en)
+
  return
 end subroutine get_v_from_p
 
