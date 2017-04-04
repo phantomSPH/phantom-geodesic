56c8b22e590803820c3f40377362362f167e756f
David Liptai <david.liptai@monash.edu>
2017-04-04 18:00:07 +1000

    Evolution outputs for xyz and vxyz are written consistently as (e.g.) xyz(1:3,:) (i.e. x,y,z position of particle 1, followed by xyz of particle 2, and so on…) Also, added some comments, additional info in header of output files, and just some general cleaning of the output module.

--- src/output.f90
+++ src/output.f90
@@ -8,17 +8,16 @@ contains
 !  (This is useful for plotting in splash, so output is always in cartesian coordinates.)
 !+
 !----------------------------------------------------------------
-subroutine write_out(time,xall,vall,np)
-   use utils_gr, only: dot_product_gr
-   use metric, only: spherical2cartesian
+subroutine write_out(time,xall,np)
+   use utils_gr,     only: dot_product_gr
+   use metric,       only: spherical2cartesian
    use metric_tools, only: coordinate_sys
-   real, intent(in) :: time
+   real,    intent(in) :: time
    integer, intent(in) :: np
-   real, dimension(1:3,np), intent(in) :: xall, vall
-   ! real, dimension(:), intent(in) :: energy_init_np,angmom_init_np
-   real :: x(1:3), v(1:3)
+   real,    intent(in) :: xall(3,np)
+   real    :: x(3)
    integer :: i
-   integer, save :: ifile = -1
+   integer, save     :: ifile = -1
    character(len=40) :: filename
 
    ifile = ifile+1
@@ -31,55 +30,67 @@ subroutine write_out(time,xall,vall,np)
       if (coordinate_sys == 'Spherical') call spherical2cartesian(xall(:,i),x)
       write(50,"(3e26.16)") x
    enddo
+   close(50)
 end subroutine write_out
 
+!----------------------------------------------------------------
+!+
+!  Write one output file containing the time evolution of total energy and  total angular momentum.
+!+
+!----------------------------------------------------------------
 subroutine write_ev(time,energy,angmom)
-   use metric, only: metric_type
    real, intent(in) :: time, energy, angmom
    integer, save :: i=0
 
-   if(metric_type=='Schwarzschild') then
    if (i==0) then
       open(unit=55, file='ev.dat',status='replace')
       write(55,*) '# Time, Energy, Angular momentum'
+      i = i+1
    else
       open(unit=55, file='ev.dat',position='append')
    endif
-   i = i+1
+
    write(55,*) time, energy, angmom
-   close(unit=55)
-   endif
+   close(55)
+
 end subroutine write_ev
 
+!----------------------------------------------------------------
+!+
+!  Write one output file containing the time evolution of position for each particle in columns.
+!+
+!----------------------------------------------------------------
 subroutine write_xyz(time,xall,np)
    use metric_tools, only: coordinate_sys
-   use metric, only: spherical2cartesian, cartesian2spherical
-   real, intent(in) :: time
+   use metric,       only: metric_type, spherical2cartesian, cartesian2spherical
+   real,    intent(in) :: time
    integer, intent(in) :: np
-   real, dimension(1:3,np) :: xall
+   real, dimension(1:3,np), intent(in) :: xall
+   real, dimension(1:3,np) :: x
    integer, save :: j=0
+   integer :: i
    logical, parameter :: write_cartesian = .true.
-   real :: x(3)
 
    if (j==0) then
       open(unit=66, file='positions.dat',status='replace')
       write(66,*) '# Number of particles (n)'
       write(66,*) np
-      write(66,*) '# First Column = Time'
-      write(66,*) '# Subsequent columns =  x1(1),x2(1),x3(1),.....,x1(n),x2(n),x3(n).'
+      write(66,*) '# First Column = Time. Subsequent columns (e.g. cartesian) =  x(1),y(1),z(1),.....,x(n),y(n),z(n).'
+      write(66,*) '# Coordinate system: ',coordinate_sys,'. Metric: ',metric_type,'. Written to file in cartesian:',write_cartesian
+      j = j+1
    else
       open(unit=66, file='positions.dat',position='append')
    endif
-   j = j+1
 
    ! Write to positions.dat file in cartesian
    if (write_cartesian) then
       if (coordinate_sys == 'Cartesian') then
          write(66,*) time, xall(1:3,:)
       else if (coordinate_sys == 'Spherical') then
-         x = xall(:,1)
-         call spherical2cartesian(xall(:,1),x)
-         write(66,*) time, x
+         do i = 1,np
+            call spherical2cartesian(xall(:,i),x(:,i))
+         enddo
+         write(66,*) time, x(1:3,:)
       else
           STOP "Please pick a coordinate system that I can write to file in"
       endif
@@ -89,34 +100,41 @@ subroutine write_xyz(time,xall,np)
       if (coordinate_sys == 'Spherical') then
          write(66,*) time, xall(1:3,:)
       else if (coordinate_sys == 'Cartesian') then
-         x = xall(:,1)
-         call cartesian2spherical(xall(:,1),x)
-         write(66,*) time, x
+         do i=1,np
+            call cartesian2spherical(xall(:,i),x(:,i))
+         enddo
+         write(66,*) time, x(1:3,:)
       else
           STOP "Please pick a coordinate system that I can write to file in"
       endif
 
    endif
-   close(unit=66)
+   close(66)
 end subroutine write_xyz
 
+!----------------------------------------------------------------
+!+
+!  Write one output file containing the time evolution of velocity for each particle in columns.
+!+
+!----------------------------------------------------------------
 subroutine write_vxyz(time,vall,np)
-   real, intent(in) :: time
+   use metric_tools, only: coordinate_sys
+   use metric,       only: metric_type
    integer, intent(in) :: np
-   real, dimension(3,np) :: vall
-   integer, save :: j
+   real,    intent(in) :: time, vall(3,np)
+   integer, save       :: j
 
    if (j==0) then
       open(unit=70, file='velocities.dat',status='replace')
       write(70,*) '# Number of particles'
       write(70,*) np
-      write(70,*) '# First Column = Time'
-      write(70,*) '# Subsequent columns =  vx(1),vx(2),...vx(n),vy(1),vy(2),...vy(n),vz(1),vz(2),...,vz(n)'
+      write(70,*) '# First Column = Time. Subsequent columns (e.g. cartesian) =  vx(1),vy(1),vz(1),...,vx(n),vy(n),vz(n)'
+      write(70,*) '# Coordinate system: ',coordinate_sys,' Metric: ',metric_type
+      j = j+1
    else
       open(unit=70, file='velocities.dat',position='append')
    endif
-   j = j+1
    write(70,*) time, vall(1:3,:)
-   close(unit=70)
+   close(70)
 end subroutine write_vxyz
 end module
